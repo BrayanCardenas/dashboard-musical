658d5a6a53c6c71bbdd66bfc60b9d8358cf9281b
Brayan Cardenas <brayansti34@gmail.com>
2025-11-25 18:05:20 -0500

    diseño inicial del commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Login from './pages/Login.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import Form from "../components/Form";
+function Login() {
+	return (
+		<div className="bg-neon h-screen">
+			<Form />
+		</div>
+	)
+}
+
+export default Login
