50ffe8262a7f0b621a7fe701272eb7299877c617
Brayan Cardenas <brayansti34@gmail.com>
2025-11-25 18:30:02 -0500

    estilos en los label y botón

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,10 +9,10 @@ function Form() {
 				<p className="text-center text-gray-500">Gestiona tu música de forma profesional</p>
 				<form action="" className="flex flex-col gap-4">
 					<label htmlFor="email" className="font-bold">Email</label>
-					<input type="email" name="email" id="email" className="border rounded-xl p-2 border-gray-300" />
+					<input type="email" name="email" id="email" className="border rounded-xl p-2 border-gray-300 text-sm focus:outline-2 focus:outline-offset-2 focus:outline-neonT" placeholder="tu@email.com" />
 					<label htmlFor="password" className="font-bold">Password</label>
-					<input type="password" name="password" id="password" className="border rounded-xl p-2 border-gray-300" />
-					<button type="submit" className="bg-neon rounded-xl font-bold p-2">Iniciar Sesión</button>
+					<input type="password" name="password" id="password" className="border rounded-xl p-2 border-gray-300 text-sm focus:outline-2 focus:outline-offset-2 focus:outline-neonT" placeholder="••••••••" />
+					<button type="submit" className="bg-neon rounded-xl font-bold p-2 cursor-pointer">Iniciar Sesión</button>
 				</form>
 			</div>
 		</div>
